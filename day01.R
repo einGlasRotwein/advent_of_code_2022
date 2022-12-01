d024c8562caf4ac836b309a5b891ada986a20362
einGlasRotwein <juliane.tkotz@gmail.com>
2022-12-01 06:32:23 +0100

    take out run time tracking

--- day01.R
+++ day01.R
@@ -1,8 +1,6 @@
 
 ## PART 1 ----------------------------------------------------------------------
 
-start <- Sys.time()
-
 day1 <- data.frame(
   calories = as.numeric(readLines("inputs/day01.txt"))
 )
@@ -14,10 +12,6 @@ calorie_sums <- by(day1$calories, day1$elf_no, sum)
 
 max(calorie_sums)
 
-pt1 <- Sys.time() - start
-
 ## PART 2 ----------------------------------------------------------------------
 
 sum(sort(calorie_sums, decreasing = TRUE)[1:3])
-
-pt2 <- Sys.time() - start
